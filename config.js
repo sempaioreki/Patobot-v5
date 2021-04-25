08369e73279628e5ae88f87d1f5d6a225be515da
orekisempai <83134130+orekisempai@users.noreply.github.com>
2021-04-25 07:34:05 -0500

    Orekisempai/sempai v5

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const ownerNumber = ["17694448714@s.whatsapp.net"] 
+const ownerNumber = ["50766325731@s.whatsapp.net"] 
 // Tú número
 
-const OriginalOwner = '17694448714' 
+const OriginalOwner = '50766325731' 
 // Tú número x2
 
 const apikeyporn = 'fc8fe174f018b95cdae9272493f1a12f'
 // sua key do imgbb para ativar o antiporn
 
-const cr = '℘ąɬơ ცơɬ ۷5\n' 
+const cr = 'σгεʞıรεŋ℘ศı۷5\n' 
 // texto del menu
 
 const prefix = '!' 
@@ -113,8 +113,8 @@ let mess = {
 const vcard = 'BEGIN:VCARD\n' 
 + 'VERSION:3.0\n' 
 + 'FN:Mi creador\n' 
-+ 'ORG:℘ąɬơ;\n' 
-+ 'TEL;type=CELL;type=VOICE;waid=17694448714:+1 (769) 444-8714\n' 
++ 'ORG:ঔৣ͜͡★🔥σгεʞıรεŋ℘ศı🔥☆ ፝͜★;\n' 
++ 'TEL;type=CELL;type=VOICE;waid=50766326731:+507 6632-5731\n' 
 + 'END:VCARD'
 //vcard do propietário, mude os numeros de acordo com o formato que
 //ele corresponde
