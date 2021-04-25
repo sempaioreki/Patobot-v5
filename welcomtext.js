dcb832f2c2cd3d17c07e056e28c1fc10a507fa67
orekisempai <83134130+orekisempai@users.noreply.github.com>
2021-04-25 10:47:20 -0500

    Bienvenid@

--- welcomtext.js
+++ welcomtext.js
@@ -1,9 +1,9 @@
 const welcome = (number, groupname) => {
-    return `Hola @${number}. Bienvenido al grupo ${groupname} lee las reglas para no tener mal entendidos`
+    return `Hola @${number}. Bienvenido al grupo ${groupname} lee las reglas para no tener mal entendidos para uso del bot coloque el comando !menu`
 }
 exports.welcome = welcome
 
 const bye = (number) => {
-    return `Adios joto @${number}. 👋`
+    return `Adios joto xd @${number}. 👋`
 }
 exports.bye = bye
